b8dab695bc0c8c92d2ddf13f7a6af26bf421bef8
Robert Machmer <robert.machmer@gmail.com>
2016-01-12 14:30:54 +0100

    Remove unused varargs

--- Graphoon/Node.lua
+++ Graphoon/Node.lua
@@ -17,7 +17,7 @@ local DEFAULT_MASS = 0.05;
 -- @param y  - The y coordinate the Node should be spawned at (optional).
 -- @param anchor - Wether the node should be locked in place or not (optional).
 --
-function Node.new( id, x, y, anchor, ... )
+function Node.new( id, x, y, anchor )
     local self = {};
 
     local px, py = x or 0, y or 0;
